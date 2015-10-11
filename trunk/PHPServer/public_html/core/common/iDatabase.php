db51b8ac62e7b221f985da4cec3dcec4503ef770
tashman <tashman@52da2821-cba8-49a6-a5f9-61225d42b614>
2015-10-11 01:12:15 +0000

    Add database interface.

---
+++ trunk/PHPServer/public_html/core/common/iDatabase.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Interface for database operations
+ *
+ * @author jnorcross
+ */
+interface iDatabase {
+    public function lastInsertId();
+    public function execute($statement, $parameters = null, $fetchData = true);
+    public function rowCount($statement, $parameters = null);
+    public function GetColumnSchema($table);
+}
