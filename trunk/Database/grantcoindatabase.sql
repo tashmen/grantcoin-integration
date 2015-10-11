d0ebe7a4553f3d84483df65a686a6e6f6dcd0bfd
tashman <tashman@52da2821-cba8-49a6-a5f9-61225d42b614>
2015-10-11 17:44:19 +0000

    Normalize the data from Bittrix and Bitstamp and create cache table to store the data; data will be refreshed if it's older than 30 minutes.

--- trunk/Database/grantcoindatabase.sql
+++ trunk/Database/grantcoindatabase.sql
@@ -8,3 +8,21 @@ CREATE TABLE `grantcoinuser` (
   `address` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The grantcoin address',
   PRIMARY KEY (`userid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Stores the user''s grantcoin address';
+
+
+--
+-- Table structure for table `marketdata`
+--
+
+DROP TABLE IF EXISTS `marketdata`;
+CREATE TABLE `marketdata` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Market ID',
+  `bid` double DEFAULT NULL COMMENT 'current bid price',
+  `ask` double DEFAULT NULL COMMENT 'current ask price',
+  `high` double DEFAULT NULL COMMENT 'Highest price for the day',
+  `low` double DEFAULT NULL COMMENT 'lowest price for the day',
+  `last` double DEFAULT NULL COMMENT 'last price paid',
+  `volume` double DEFAULT NULL COMMENT 'number of trades',
+  `lastupdated` datetime NOT NULL COMMENT 'date and time of when the record was last updated',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Storage for market data to cache the data to reduce requests';
--- trunk/PHPServer/public_html/core/grantcoin/Bittrex.php
+++ trunk/PHPServer/public_html/core/grantcoin/Bittrex.php
@@ -11,6 +11,17 @@ class Bittrex {
     private static $serverLocation = 'https://bittrex.com/';
     private static $publicApiPath = 'api/v1.1/public/';
     private static $btcGRTMarket = 'btc-grt';
+    private static $connection = null;
+    
+    
+    /*
+     * Set a connection for the class to use for cachine data in the provided database
+     * @param connection - The database connection object
+     */
+    public static function SetConnection(iDatabase $connection){
+        self::$connection = $connection;
+    }
+            
     
     /*
      * Retrieves the asking price for the current exchange of BTC to GRT
@@ -22,8 +33,8 @@ class Bittrex {
     }
     
      /*
-     * Retrieves the bid price for the current exchange of BTC to GRT
-     * @return - The current bid price in BTC for 1 GRT
+      * Retrieves the bid price for the current exchange of BTC to GRT
+      * @return - The current bid price in BTC for 1 GRT
      */
     public static function GetBidGRTToBTCValue()
     {
@@ -34,19 +45,13 @@ class Bittrex {
      * Retrieves the BTC-GRT Market Summary
      * @return - The current market summary for BTC-GRT in the following form:
      *  {
-            "MarketName" : "BTC-GRT",
-            "High" : 0.00000128,
-            "Low" : 0.00000117,
-            "Volume" : 88714.48403787,
-            "Last" : 0.00000117,
-            "BaseVolume" : 0.10871928,
-            "TimeStamp" : "2015-10-01T22:05:26.62",
-            "Bid" : 0.00000117,
-            "Ask" : 0.00000133,
-            "OpenBuyOrders" : 28,
-            "OpenSellOrders" : 89,
-            "PrevDay" : 0.00000128,
-            "Created" : "2015-05-15T21:25:04.71"
+            "high" : 0.00000128,
+            "low" : 0.00000117,
+            "volume" : 88714.48403787,
+            "last" : 0.00000117,
+            "bid" : 0.00000117,
+            "ask" : 0.00000133,
+            "lastupdated" : "1990-12-20 13:34:54"
         }
      */
     public static function GetBTCGRTMarketSummary()
@@ -60,7 +65,7 @@ class Bittrex {
      */
     private static function GetAskPrice($market)
     {
-        return self::GetMarketSummary($market)->Ask;
+        return self::GetMarketSummary($market)->ask;
     }
     
     /*
@@ -69,11 +74,12 @@ class Bittrex {
      */
     private static function GetBidPrice($market)
     {
-        return self::GetMarketSummary($market)->Bid;
+        return self::GetMarketSummary($market)->bid;
     }
     
     /* obtains the markey summary of any market
-     * @market - The type of market to retrieve from Bittrex
+     * @param market - The type of market to retrieve from Bittrex
+     * @param connection - The database connection object
      * throws Exception if the request fails
      */
     
@@ -81,54 +87,87 @@ class Bittrex {
     {
         if(self::$marketSummaryDictionary[$market] == NULL)
         {
-            //Request
-            //https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-grt
-
-            //Response 
-            /*
+            if(self::$connection != null)
             {
-                "success" : true,
-                "message" : "",
-                "result" : [{
-                    "MarketName" : "BTC-GRT",
-                    "High" : 0.00000128,
-                    "Low" : 0.00000117,
-                    "Volume" : 88714.48403787,
-                    "Last" : 0.00000117,
-                    "BaseVolume" : 0.10871928,
-                    "TimeStamp" : "2015-10-01T22:05:26.62",
-                    "Bid" : 0.00000117,
-                    "Ask" : 0.00000133,
-                    "OpenBuyOrders" : 28,
-                    "OpenSellOrders" : 89,
-                    "PrevDay" : 0.00000128,
-                    "Created" : "2015-05-15T21:25:04.71"
-                }]
+                $results = MarketData::GetMarketData(self::$connection, $market);
+                if($results != null)
+                {
+                    self::$marketSummaryDictionary[$market] = $results;
+                }
             }
-
-            */
-
-            $ch = curl_init(self::$serverLocation . self::$publicApiPath . "getmarketsummary?market=" . $market);
-
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-            curl_setopt($ch, CURLOPT_TIMEOUT, '10');
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-json", "Accept: application/x-json"));
-
-            $json_response = curl_exec($ch);
-            if ($curl_error = curl_error($ch)) {
-                throw new Exception('Bittrex::GetMarketSummary error: ' . $curl_error);
-            }
-            curl_close($ch);
-
-            $response = json_decode($json_response);
-            if($response->success)
+            
+            if(self::$marketSummaryDictionary[$market] == NULL)
             {
-                self::$marketSummaryDictionary[$market] = $response->result[0];
+            
+                //Request
+                //https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-grt
+
+                //Response 
+                /*
+                {
+                    "success" : true,
+                    "message" : "",
+                    "result" : [{
+                        "MarketName" : "BTC-GRT",
+                        "High" : 0.00000128,
+                        "Low" : 0.00000117,
+                        "Volume" : 88714.48403787,
+                        "Last" : 0.00000117,
+                        "BaseVolume" : 0.10871928,
+                        "TimeStamp" : "2015-10-01T22:05:26.62",
+                        "Bid" : 0.00000117,
+                        "Ask" : 0.00000133,
+                        "OpenBuyOrders" : 28,
+                        "OpenSellOrders" : 89,
+                        "PrevDay" : 0.00000128,
+                        "Created" : "2015-05-15T21:25:04.71"
+                    }]
+                }
+
+                */
+
+                $ch = curl_init(self::$serverLocation . self::$publicApiPath . "getmarketsummary?market=" . $market);
+
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+                curl_setopt($ch, CURLOPT_TIMEOUT, '10');
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/x-json", "Accept: application/x-json"));
+
+                $json_response = curl_exec($ch);
+                if ($curl_error = curl_error($ch)) {
+                    throw new Exception('Bittrex::GetMarketSummary error: ' . $curl_error);
+                }
+                curl_close($ch);
+
+                $response = json_decode($json_response);
+                if ($response->success) {
+                    $response = $response->result[0];
+                    if(self::$connection != null)
+                    {
+                        self::$marketSummaryDictionary[$market] = MarketData::CreateUpdateMarketData(self::$connection, $market, $response->Bid, $response->Ask, $response->High, $response->Low, $response->Last, $response->Volume);
+                    }
+                    else {
+                        self::$marketSummaryDictionary[$market] = self::NormalizeResponse($response);
+                    }
+                } else {
+                    throw new Exception('Bittrex::GetMarketSummary error: ' . $response->message);
+                }
             }
-            else throw new Exception ('Bittrex::GetMarketSummary error: ' . $response->message);
         }
         
         return self::$marketSummaryDictionary[$market];
+    }
+    
+    private static function NormalizeResponse($response)
+    {
+        $object = new stdClass();
+        $object->bid = $response->Bid;
+        $object->ask = $response->Ask;
+        $object->high = $response->High;
+        $object->low = $response->Low;
+        $object->last = $response->Last;
+        $object->volume = $response->Volume;
+        $dateTime = new DateTime();
+        $object->lastupdated = $dateTime->format('Y-m-d H:i:s');
     }
 }
