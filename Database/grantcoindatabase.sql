9b807ad41d6207b2cbbe765135535edcccaf5a41
tashman <tashmen@users.noreply.github.com>
2015-10-19 23:06:28 +0000

    Add licensing.

--- Database/grantcoindatabase.sql
+++ Database/grantcoindatabase.sql
@@ -1,3 +1,23 @@
+/*
+    Copyright 2015 Jon Norcross 
+
+    This file is part of GrantCoin Integration.
+
+    GrantCoin Integration is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GrantCoin Integration is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GrantCoin Integration.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 --
 -- Table structure for table `grantcoinuser`
 --
--- PHPServer/public_html/core/grantcoin/GrantCoinUserTransaction.php
+++ PHPServer/public_html/core/grantcoin/GrantCoinUserTransaction.php
@@ -1,5 +1,22 @@
 <?php
+/*
+    Copyright 2015 Jon Norcross 
 
+    This file is part of GrantCoin Integration.
+
+    GrantCoin Integration is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GrantCoin Integration is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GrantCoin Integration.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /**
  * Class for tracking, storing and applying GrantCoinUser Transactions
  *
